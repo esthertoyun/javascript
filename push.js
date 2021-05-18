a96b5a2b15bb462c22d467df5aaa13ceb5f8c1a7
ESTHER'S-PC <adeniranesther@gmail.com>
2021-05-17 19:03:28 -0700

    assignment

---
+++ push.js
@@ -0,0 +1,30 @@
+
+ const diff = (a,b) => {
+     
+     return a-b}
+ console.log(diff(40, 10))
+
+ a=prompt( "Enter a Number")
+if (a%2==0){
+    alert("It is an even number")}
+else {
+    alert("It is an odd number")}
+
+const diff = (a,b) => {
+     return a-b}
+console.log(diff(40, 10))
+
+const diff = (c,d) => {
+    return c*d}
+console.log(diff(7, 8))
+
+const div = (e,f) => {
+    return e/f}
+
+console.log(div(80, 8))
+let a=(prompt("Your name"))
+let b= a.toUpperCase()
+document.write(b)
+ 
+ 
+ 
